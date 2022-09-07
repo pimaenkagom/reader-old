2b6438a4b60f68941630f19f47c6b051cc0eb865
Miena Basta Badres <dev@mbadres.de>
2022-09-08 00:26:31 +0200

    Load an asset in main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:reader/core/settings.dart';
 import 'package:reader/ui/home/home.dart';
 
 void main() {
+
+  // See https://api.flutter.dev/flutter/widgets/WidgetsFlutterBinding/ensureInitialized.html
+  WidgetsFlutterBinding.ensureInitialized();
+
+  loadAsset().then((settings) => { print(settings) });
+
   runApp(const MyApp());
 }
